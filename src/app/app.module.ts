f567869033c8ba36820532b58c28e1dae816a7c2
Abimael Santos <abimael.web@gmail.com>
2021-02-22 21:23:28 -0300

    ajuste menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterTableComponent } from './shared/components/filter-table/filter-table.component';
+import { TableRankComponent } from './modules/table-rank/table-rank.component';
 
+const routes: Routes = [
+  { path: 'first-component', component: TableRankComponent },
+  { path: 'second-component', component: FilterTableComponent },
+]; // sets up routes constant where you define your routes
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterTableComponent,
+    TableRankComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/modules/table-rank/table-rank.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-rank',
+  templateUrl: './table-rank.component.html',
+  styleUrls: ['./table-rank.component.scss']
+})
+export class TableRankComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
